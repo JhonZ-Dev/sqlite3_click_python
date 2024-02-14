26ce66e6481523110e062be1f47c506f43eb3f51
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-13 22:57:35 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,4 @@
-import sqlite3
-import click
-# Función para crear la base de datos
-def crear_base_datos():
-    conn = sqlite3.connect('tareas.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS tareas
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, descripcion TEXT, completada INTEGER)''')
-    conn.commit()
-    conn.close()
+
 
 # Función para agregar una nueva tarea
 @click.command()
