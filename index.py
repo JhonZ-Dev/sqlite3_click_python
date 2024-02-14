36439d770917382b5bfd754d2df9408a9e5a2f77
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-02-13 22:57:56 -0500

    Update index.py

--- index.py
+++ index.py
@@ -9,17 +9,7 @@ def crear_base_datos():
     conn.commit()
     conn.close()
 
-# Función para agregar una nueva tarea
-@click.command()
-@click.option('--nombre', prompt='Nombre de la tarea', help='Nombre de la tarea.')
-@click.option('--descripcion', prompt='Descripción de la tarea', help='Descripción de la tarea.')
-def agregar_tarea(nombre, descripcion):
-    conn = sqlite3.connect('tareas.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO tareas (nombre, descripcion, completada) VALUES (?, ?, ?)", (nombre, descripcion, 0))
-    conn.commit()
-    click.echo('Tarea agregada correctamente.')
-    conn.close()
+
 # Función para ver todas las tareas
 @click.command()
 def ver_tareas():
